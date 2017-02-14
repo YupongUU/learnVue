5a9a54f97bb18babacfc51f86143ae2fb0917209
YupongUU <cnfeat@gmail.com>
2017-02-14 21:52:02 +0800

    TodoList

---
+++ task2/app.js
@@ -0,0 +1,38 @@
+import  Vue from 'vue'
+var app = new Vue({
+    el: '#app',
+    data: {
+        newTodo: '',
+        todoList: []
+    },
+    created: function(){
+        // onbeforeunload文档：https://developer.mozilla.org/zh-CN/docs/Web/API/Window/onbeforeunload
+        window.onbeforeunload = ()=>{
+            let dataString = JSON.stringify(this.todoList) // JSON 文档: https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/JSON
+            window.localStorage.setItem('myTodos', dataString) // 看文档https://developer.mozilla.org/zh-CN/docs/Web/API/Window/localStorage
+        }
+
+        let oldDataString = window.localStorage.getItem('myTodos')
+        let oldData = JSON.parse(oldDataString)
+        this.todoList = oldData || []
+
+    },
+    methods: {
+        addTodo: function(){
+            if(this.newTodo!==''){
+                this.todoList.push({
+                    title: this.newTodo,
+                    createdAt: new Date(),
+                    done: false // 添加一个 done 属性
+                })
+                this.newTodo = ''
+            }
+
+
+        },
+        removeTodo: function(todo){
+            let index = this.todoList.indexOf(todo)
+            this.todoList.splice(index,1)
+        }
+    }
+})
---
+++ task2/bar.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 2017.02.14.
+ */
+export default function bar() {
+    alert('Hello Webpack!')
+}
